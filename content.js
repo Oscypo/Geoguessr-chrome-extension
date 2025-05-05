39cb17e3cc5139e3f9e1621a19dac4d24892a0f7
oscyp <michalwietrzynski1@gmail.com>
2025-05-05 20:39:21 +0200

    changed the system to mutation observer

--- content.js
+++ content.js
@@ -1,20 +1,53 @@
 //blink mode geoguessr
-let roundTime = 1.5;
+let roundTime = 1;
+let bufferTime = 2;
+let currentlyBlank = false;
+let panoramaFound = false;
 //set roundTime in seconds
 
-const interval = setInterval(() => {
-	const panorama = document.querySelector("[data-qa=panorama]");
-	if (panorama) {
-		clearInterval(interval);
-		setTimeout(() => {
-			blank(panorama);
-			console.log("wykonane");
-		}, roundTime * 1000);
-	}
-}, 1);
+// function SearchingInterval() {
+// 	const interval = setInterval(() => {
+// 		console.log("interval ticking");
+// 		const panorama = document.querySelector("[data-qa=panorama]");
+// 		if (panorama) {
+// 			clearInterval(interval);
+// 			setTimeout(() => {
+// 				blank(panorama);
+// 				console.log("wykonane");
+// 			}, roundTime * 1000);
+// 		}
+// 	}, 1);
+// }
 
+// if (document.querySelector("[class*=result-layout]")) SearchingInterval();
 function blank(panorama) {
 	panorama.style.filter = "brightness(0%)";
-	console.log("Panorama found and styled.");
+	console.log("blank");
 	return;
 }
+function blink(panorama) {
+	panorama.style.filter = "brightness(100%)";
+	console.log("blinked");
+	return;
+}
+
+const mutation = new MutationObserver((mutationsList, observer) => {
+	const panorama = document.querySelector("[data-qa=panorama]");
+	const resultLayout = document.querySelector('[class*="result-layout"]');
+	if (panorama && !panoramaFound) {
+		panoramaFound = true;
+		console.log("panorama znaleziona");
+		blank(panorama);
+		setTimeout(() => {
+			blink(panorama);
+			setTimeout(() => {
+				blank(panorama);
+			}, roundTime * 1000);
+		}, bufferTime * 1000);
+	}
+	if (!panorama && panoramaFound) {
+		console.log("panorama not found, setting panoramaFound to false");
+		panoramaFound = false;
+	}
+});
+mutation.observe(document.body, { childList: true, subtree: true });
